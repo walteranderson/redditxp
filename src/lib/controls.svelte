880d2ae6891d38fba7f83df21cba952c55ed1ffb
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-09-01 22:18:48 -0500

    starting controls

---
+++ src/lib/controls.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { CircleEllipsis } from "lucide-svelte";
+
+  export let time; // ms
+
+  let open = false;
+  const onClick = () => {
+    open = !open;
+  };
+</script>
+
+{#if open}
+  <div class="menu">
+    <input type="number" bind:value={time} />
+  </div>
+{/if}
+
+<button on:click={onClick}>
+  <CircleEllipsis size={32} />
+</button>
+
+<style lang="postcss">
+  button {
+    position: absolute;
+    bottom: 1rem;
+    cursor: pointer;
+    left: 1rem;
+    color: white;
+    background: none;
+    border: 0;
+  }
+
+  .menu {
+    position: absolute;
+    bottom: 4.5rem;
+    left: 1rem;
+    background-color: rgba(255, 255, 255, 0.75);
+
+    padding: 0.75rem 1rem;
+    border-radius: 0.25rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+</style>
--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -1,83 +1,89 @@
 <script>
-	import { onMount } from "svelte";
-	import { getPosts } from "$lib/reddit";
-	import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import { getPosts } from "$lib/reddit";
+  import { page } from "$app/stores";
+  import Controls from "$lib/controls.svelte";
 
-	export let data;
+  export let data;
 
-	let current = 0;
-	let queue = [];
+  let current = 0;
+  let queue = [];
 
-	data.posts.forEach(addToQueue);
+  data.posts.forEach(addToQueue);
 
-	function addToQueue(post) {
-		if (post.album.length) {
-			queue.push(...post.album);
-		} else {
-			queue.push(post);
-		}
-	}
+  function addToQueue(post) {
+    if (post.album.length) {
+      queue.push(...post.album);
+    } else {
+      queue.push(post);
+    }
+  }
 
-	let after = data.after;
-	async function loadMore() {
-		const searchParams = $page.url.searchParams;
-		searchParams.set("after", after);
-		const res = await getPosts($page.params.path, searchParams);
-		res.posts.forEach(addToQueue);
-		after = res.after;
-	}
+  let after = data.after;
+  async function loadMore() {
+    const searchParams = $page.url.searchParams;
+    searchParams.set("after", after);
+    const res = await getPosts($page.params.path, searchParams);
+    res.posts.forEach(addToQueue);
+    after = res.after;
+  }
 
-	// ----
-	let time = 5000;
-	let ref;
-	onMount(() => {
-		start();
-		return () => {
-			stop();
-		};
-	});
-	function start() {
-		console.log("start");
-		clearInterval(ref);
-		ref = setInterval(tick, time);
-	}
-	function stop() {
-		console.log("stop");
-		clearInterval(ref);
-	}
-	function tick() {
-		console.log("tick", { current, queue });
-		current++;
-		if (current + 1 === queue.length) {
-			loadMore();
-		}
-	}
-	// ----
+  // ----
+  let time = 30000;
+  let ref;
+  onMount(() => {
+    start();
+    return () => {
+      stop();
+    };
+  });
+  function start() {
+    console.log("start");
+    clearInterval(ref);
+    ref = setInterval(tick, time);
+  }
+  function stop() {
+    console.log("stop");
+    clearInterval(ref);
+  }
+  function tick() {
+    console.log("tick", { current, queue });
+    current++;
+    if (current + 1 === queue.length) {
+      loadMore();
+    }
+  }
+  // ----
+
+  $: console.log({ time });
 </script>
 
+<Controls bind:time />
+
 {#if queue[current]}
-	{@const p = queue[current]}
-	{#if p.video}
-		<video autoplay loop>
-			<track kind="captions" />
-			<source src={p.video.src} />
-		</video>
-	{:else}
-		<div class="img" style={`background-image:url(${p.url})`} />
-	{/if}
+  {@const p = queue[current]}
+  {#if p.video}
+    <video autoplay loop>
+      <track kind="captions" />
+      <source src={p.video.src} />
+    </video>
+  {:else}
+    <div class="img" style={`background-image:url(${p.url})`} />
+  {/if}
 {/if}
 
 <style>
-	:global(html, body) {
-		background-color: black;
-		height: 100%;
-		margin: 0;
-	}
-	.img {
-		width: 100%;
-		height: 100%;
-		background-size: contain;
-		background-position: center;
-		background-repeat: no-repeat;
-	}
+  :global(html, body) {
+    background-color: black;
+    height: 100%;
+    margin: 0;
+    font-family: Helvetica, Arial, sans-serif;
+  }
+  .img {
+    width: 100%;
+    height: 100%;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
 </style>
