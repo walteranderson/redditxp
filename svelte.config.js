73ad54c8e982d543ee66682f75a17fc19300d0bf
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-10-31 15:00:27 -0500

    adding netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    }),
+    adapter: adapter(),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
